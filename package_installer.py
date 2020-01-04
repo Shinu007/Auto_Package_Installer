34084b7b3b9e3f302cf2e3b68a7e089aedfed05f
sophia <sophia@pop-os.localdomain>
2020-01-04 17:25:14 +0530

    First Commit

---
+++ package_installer.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+import os
+import json
+import time
+
+user = os.getenv("SUDO_USER")
+
+if user is None:
+    print("run this with sudo !!!!")
+    exit()
+
+installation_packages = json.load(open('Packages.json'))
+
+os.system("sudo apt update -y ")
+os.system("sudo apt upgrade -y")
+
+for packages in installation_packages:
+
+    os.system("sudo apt install %s -y"%(packages))
+
+time_end = time.time() + 11
+os.system("figlet 'Your system will reboot in 10 Seconds press Ctrl+c to stop the reboot' ")
+
+while time.time() < time_end:
+    yes = True
+
+os.system("figlet Yes")
